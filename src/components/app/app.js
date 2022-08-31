62713a8a4fc8fefce9d71a2ce9cbfe8da101a3f6
Tyrsa Vladyslav <anonimus.v34@gmail.com>
2022-08-31 18:34:01 +0300

    search panel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component{
                 {name: 'Jhon S.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Alex M.', salary: 4000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 1500, increase: false, rise: false, id: 3},
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -56,20 +57,37 @@ class App extends Component{
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
+
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
     
                 <EmployeeList 
-                data={this.state.data}
+                data={visibleData}
                 onDelete={this.deleteItem}
                 onToggleProp={this.onToggleProp}/>
                 <EmployeeAddForm onAdd={this.addItem}/>
